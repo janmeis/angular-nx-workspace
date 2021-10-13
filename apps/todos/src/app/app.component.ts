d33342cc6f6c2e65c8883fabee69014b7606a09e
Jan Meissner <janmeis@yahoo.co.uk>
2021-10-13 11:27:19 +0200

    initial commit

---
+++ apps/todos/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+interface Todo {
+  title: string;
+}
+@Component({
+  selector: 'angular-nx-workspace-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'todos';
+  todos: Todo[] = [];
+
+  constructor(private http: HttpClient) {
+    this.fetch();
+  }
+
+  fetch() {
+    this.http.get<Todo[]>('/api/todos').subscribe((t) => (this.todos = t));
+  }
+
+  addTodo() {
+    this.http.post('/api/addTodo', {}).subscribe(() => {
+      this.fetch();
+    });
+  }
+}
